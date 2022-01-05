70f84a23f4cb601a6afa25cf19188bf6ba6843e2
loorr <zjianfa@qq.com>
2022-01-05 19:26:16 +0800

    update

--- market-collector-tushare/src/main/java/com/tove/market/tushare/config/DayDataTablesPreciseShardingAlgorithm.java
+++ market-collector-tushare/src/main/java/com/tove/market/tushare/config/DayDataTablesPreciseShardingAlgorithm.java
@@ -5,17 +5,16 @@ import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 
 import java.util.Collection;
 
+/**
+ * @author user
+ */
 public class DayDataTablesPreciseShardingAlgorithm implements PreciseShardingAlgorithm<String> {
 
     @Override
-    public String doSharding(Collection<String> collection, PreciseShardingValue<String> preciseShardingValue) {
-        // 分片字段值
+    public String doSharding(Collection<String> tableNames, PreciseShardingValue<String> preciseShardingValue) {
         String value = preciseShardingValue.getValue();
-        // 现在算法是:%2 求余如果是0则xmjbq_user0,如果是1则xmjbq_user1。但是由于id是字符串而且是很长的，所以截取最后一位然后转为Integer类型再求余
-
-        Integer number = Integer.valueOf(value);
-        int result = number % 5;
-        for (String s : collection) {
+         int result = Integer.valueOf(value) % 15;
+        for (String s : tableNames) {
             if(s.endsWith(result+"")){
                 return s;
             }
